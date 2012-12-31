4b92831e7406442177a52218dddb0f776e3a7105
janebeta7 <alba@albagcorral.com>
2012-12-31 04:41:16 +0100

    updating

--- gen/com/janebeta7/aPitagoras/R.java
+++ gen/com/janebeta7/aPitagoras/R.java
@@ -14,26 +14,31 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int fade=0x7f060000;
-        public static final int info=0x7f060002;
-        public static final int save=0x7f060001;
+        public static final int color=0x7f070001;
+        public static final int fade=0x7f070000;
+        public static final int save=0x7f070002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class menu {
-        public static final int app_menu=0x7f050000;
+        public static final int app_menu=0x7f060000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int folder_path=0x7f040006;
-        public static final int main_screen=0x7f040001;
-        public static final int menu_fade=0x7f040005;
-        public static final int menu_info=0x7f040004;
-        public static final int menu_save=0x7f040003;
-        public static final int msg_saveImageFail=0x7f040009;
-        public static final int msg_saveImageOk=0x7f040008;
-        public static final int name_path=0x7f040007;
-        public static final int splash_screen=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int ext_path=0x7f050009;
+        public static final int folder_path=0x7f050007;
+        public static final int main_screen=0x7f050001;
+        public static final int menu_color=0x7f050004;
+        public static final int menu_fade=0x7f050005;
+        public static final int menu_save=0x7f050003;
+        public static final int menu_settings=0x7f050006;
+        public static final int msg_saveImageFail=0x7f05000b;
+        public static final int msg_saveImageOk=0x7f05000a;
+        public static final int name_path=0x7f050008;
+        public static final int splash_screen=0x7f050002;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
--- src/com/janebeta7/aPitagoras/aPitagoras.java
+++ src/com/janebeta7/aPitagoras/aPitagoras.java
@@ -1,300 +1,327 @@
 package com.janebeta7.aPitagoras;
 
 import java.io.File;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import processing.core.PApplet;
 import com.janebeta7.aPitagoras.R;
+import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.os.Environment;
+import android.preference.PreferenceActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.widget.Toast;
 
-
-
+@SuppressLint("SimpleDateFormat")
 public class aPitagoras extends PApplet {
+	private int mInitialColor = 0xFFFFFFFF;
+	protected static final int EDIT_PREFS = 1;
+	/*
+	 * --------------------------------------------------------------------------
+	 * ------ Alba G.Corral (www.albagcorral.com)
+	 * 
+	 * --------------------------------------------------------------------------
+	 * ------
+	 */
 
-/*
---------------------------------------------------------------------------------
- Alba G.Corral  (www.albagcorral.com)
- 
- --------------------------------------------------------------------------------
- 
- */
+	/* --- android variables --- */
+	NotificationManager gNotificationManager;
+	Notification gNotification;
+	long[] gVibrate = { 0, 50 };
 
-/* --- android variables   --- */
-NotificationManager gNotificationManager;
-Notification gNotification;
-long[] gVibrate = {0,50};
+	/* --- processing variables --- */
 
-
-/* ---  processing variables   ---  */
-
-String COLORES = "set" ;// RUTA donde se encuentra la paleta de colores
-Fade fade;
-//MovieMaker mm;  // Declare MovieMaker object
-
-int radio = 100;
-boolean hazFade= false;
-
-int espacio = 1;
-int NUM =1; //NUMERO DE CIRCULOS
-Circulo[] w = new Circulo[NUM]; //array de cienpies
-int colorFade, colorFadeInv;
-Circulo circulo ;
-int z = 0;
-boolean grabamos = false;
-public void setup() 
-{
-  smooth();
- //ponemos el ancho y el alto de laventana
-  println("--SETUP----");
-  fade = new Fade();
-  inicia();
-  background(0);
-}
-public void inicia() {
-  for (int i=0;i<NUM;i++) {
-
-    w[i] = new Circulo(PApplet.parseInt(random(radio)), mouseX, mouseY); //nuevo circulo
-  }
-}
-public void draw() 
-{ 
- // frameRate(30);
-  fade.render();
-
-  if (grabamos) {  
-    //mm.addFrame();
-  } // Add window's pixels to movie
-
-  for (int z=0;z<NUM;z++) {
-
-    if (w[z].dibujado==false) {
-
-      w[z].draw(255);//llamamos al metodo de la clase Wurm.draw();
-    }
-  }
-}
-
-
-//clase que controla el fade in y el fade out
-class Fade{
-  float fadeTime;
-  boolean fadeIn=false,fadeOut=false,on=false;  
-  Fade(){
-    println("init Fade");
-  }
-  public void render(){
-
-
-    if(fadeIn) {
-      if(fadeTime<1) fadeTime=fadeTime+1.0f/100.0f;
-      else { // done fading in
-        fadeTime=1;
-        on=true;
-        fadeIn=false;
-      }
-    }
-    if(fadeOut) {
-      if(fadeTime>0) fadeTime=fadeTime-1.0f/100.0f;
-      else { // done fading in
-        fadeTime=0;
-        on=false;
-        fadeOut=false;
-      }
-    }
-    fade();
-  }
-  public float getFade(){
-    return fadeTime;
-  }
-  public void setFade(){
-    if(on==false) fadeIn=true;
-    else fadeOut=true;
-  }
- private void fade(){
-    fill(0,fadeTime*10);
-    rect(0,0,width,height);
-   }
-}
-
-class Circulo {
-  //
-
-
-  float a = 0.0f  ;
-  float inc = TWO_PI/90;
-
-  boolean dibujado= false; 
-  int radio, Ax, Ay;
-  //constructor de la clase
-  Circulo (int _radio, int _Ax, int _Ay) {
-    this.radio = _radio;
-    this.Ax = _Ax;
-    this.Ay =_Ay;
-  }
-  public void draw(int colorr) {
-    //variante while dibuja de golpe
-    while (a <  TWO_PI) {
-      dibujaCirculo();
-      //  rotate(0.5);
-      dibujaElipse(colorr);
-      a = a + inc;
-    }
-    a =0;
-    radio++;
-
-    if (radio > 400) {
-      dibujado = true;
-    }
-  }
-
-  public void dibujaCirculo() {
-
-    // strokeWeight(1); 
-    //    ahora recorremos la longitud de r para ir creando puntos que recorran todo el areadel circulo
-    for (int i = 0; i < radio; i = i+100) {
-
-      float xx = cos(a)*i+Ax; 
-      float yy = sin(a)*i+Ay; 
-      //float xx2 = cos(a)*(i+int(random(50)))+Ax; 
-      //float yy2 = sin(a)*(i+int(random(100)))+Ay; 
-      float xx2 = cos(a)*(i+10)+Ax; 
-      float yy2 = sin(a)*(i+10)+Ay; 
-      stroke(0xffffffff, 20);
-      //strokeWeight(1);
-      //  ellipse(xx,yy,10,10);
-      //line(xx,yy,xx2,yy2);
-      noFill();
-      float cx1 =  xx+PApplet.parseInt(random(20));
-      float cx2 = xx2+PApplet.parseInt(random(20));
-      float cy1 = yy+PApplet.parseInt(random(20));
-      float cy2 = yy2+PApplet.parseInt(random(20));
-      //rect(xx, yy, xx2, yy2);
-      //bezier(xx, yy, cx1, cy1, cx2, cy2, xx2, yy2);
-
-      //   noStroke();
-    }
-  }
-  public void dibujaElipse(int colorr) {
-    noStroke();
-    int tam = 2;
-    fill(colorr);
-    ellipse(cos(a)*radio+Ax, sin(a)*radio+Ay, tam, tam);
-  }
-}
-
-
-
-public boolean surfaceTouchEvent(MotionEvent event) {
-  // If user touches the screen, trigger vibration notification:
-  //gNotificationManager.notify(1, gNotification);
-	//getScreen();
-  return super.surfaceTouchEvent(event);
-}
-public void mousePressed() {
- // gNotificationManager.notify(1, gNotification);
-  inicia();
-}
-
-//-----------------------------------------------------------------------------------------
-// Override the parent (super) Activity class:
-// States onCreate(), onStart(), and onStop() aren't called by the sketch.  Processing is entered
-// at the 'onResume()' state, and exits at the 'onPause()' state, so just override them as needed:
-
-public void onResume() {
-  super.onResume();
-    println("RESUMED! (Sketch Entered...)");
-  // Create our Notification Manager:
-  gNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-  // Create our Notification that will do the vibration:
-  gNotification = new Notification();
-  gNotification.defaults |= Notification.DEFAULT_SOUND;
-  gNotification.vibrate = gVibrate;
-  
-
-}
-
-
-
-/*-----------------------------------------------------------------------------*/
-/*------------------------ ADD IN ECLIPSE--------------------------------------*/
-/*-----------------------------------------------------------------------------*/
-
-/*------------------------ getScreen-----------------------------------*/
-private void getScreen(){
+	Fade fade;
 	
-	String state = Environment.getExternalStorageState();
+	int radio = 100;
+	boolean hazFade = false;
+	int Ax;
+	int Ay;
+	int espacio = 1;
+	int NUM = 4; // NUMERO DE CIRCULOS
+	Circulo[] w = new Circulo[NUM]; // array de cienpies
+	int colorFade, colorFadeInv;
+	Circulo circulo;
+	int z = 0;
+	int contCirculos = 0;
+	boolean iniciamos = false;
 
-	if (Environment.MEDIA_MOUNTED.equals(state)) {
-	    /* como podemos leer la tarjeta grabamos*/
-	    String folder_path = getResources().getString(R.string.folder_path);
-		String name_path = getResources().getString(R.string.name_path);
-		
-		File extStore = Environment.getExternalStorageDirectory();
-		String SD_PATH = extStore.getAbsolutePath()+folder_path+name_path;
-		save(SD_PATH);
-		println("------salvamos imagen------"+SD_PATH);
-		Toast.makeText(getApplicationContext(), 
-				getResources().getString(R.string.msg_saveImageOk)+SD_PATH, Toast.LENGTH_LONG).show();
-		//TODO para que sea visible en galery
+	public void setup() {
+		smooth();
+		Ax = displayWidth;
+		Ay = displayHeight;
+		fade = new Fade();
+		background(0);
+	}
 
+	public void inicia() {
 
+		//println("contCirculos" + contCirculos);
+		if (contCirculos == (NUM))
+			contCirculos = 0;
+		w[contCirculos] = new Circulo(PApplet.parseInt(random(radio)),
+				PApplet.parseInt(mouseX), PApplet.parseInt(mouseY)); // nuevo
+																		// circulo
+		contCirculos++; // note the use of modulo
+	}
 
-	    /* */
-	    
-	    
-	} else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-		 /* esta solo modo lectura*/
-	    // We can only read the media
-		Toast.makeText(getApplicationContext(), 
-				getResources().getString(R.string.msg_saveImageFail), Toast.LENGTH_LONG).show();
-	} else {
-		Toast.makeText(getApplicationContext(), 
-				getResources().getString(R.string.msg_saveImageFail), Toast.LENGTH_LONG).show();
+	public void draw() {
+		fade.render();
+		for (int z = 0; z < contCirculos; z++) {
+			//println("z:" + z);
+			if (w[z].dibujado == false)
+				w[z].draw(255);
+		}
 	}
 	
-	
-	
-	
-}
+	// clase que controla el fade in y el fade out
+	class Fade {
+		float fadeTime;
+		boolean fadeIn = false, fadeOut = false, on = false;
 
-/*------------------------ CREAMOS ACCIONES DE BOTON MENU-----------------------------------*/
-@Override
-public boolean onCreateOptionsMenu(Menu menu) {
-	MenuInflater inflater = getMenuInflater();
-	inflater.inflate(R.menu.app_menu, menu);
-	return true;
-}
- /**
- * Opens the file manager to pick a directory.
- */
+		Fade() {
+			println("init Fade");
+		}
 
-@Override
-public boolean onOptionsItemSelected(MenuItem item) {
-	// Handle item selection
-	switch (item.getItemId()) {
-	case R.id.info:
-		Toast.makeText(this, "aPitagoras 1.0 by janebeta7", Toast.LENGTH_SHORT).show();
-		return true;
-	case R.id.fade:
-		 fade.setFade();
-		return true;
-	case R.id.save:
-		getScreen();
-		return true;
-	
-	default:
-		return super.onOptionsItemSelected(item);
+		public void render() {
+
+			if (fadeIn) {
+				if (fadeTime < 1)
+					fadeTime = fadeTime + 1.0f / 100.0f;
+				else { // done fading in
+					fadeTime = 1;
+					on = true;
+					fadeIn = false;
+				}
+			}
+			if (fadeOut) {
+				if (fadeTime > 0)
+					fadeTime = fadeTime - 1.0f / 100.0f;
+				else { // done fading in
+					fadeTime = 0;
+					on = false;
+					fadeOut = false;
+				}
+			}
+			fade();
+		}
+
+		public float getFade() {
+			return fadeTime;
+		}
+
+		public void setFade() {
+			if (on == false)
+				fadeIn = true;
+			else
+				fadeOut = true;
+		}
+
+		private void fade() {
+			fill(0, fadeTime * 10);
+			rect(0, 0, width, height);
+		}
 	}
-}
-  public int sketchWidth() { return displayWidth; }
-  public int sketchHeight() { return displayHeight; }
-}
+
+	class Circulo {
+		  //
 
 
+		  float a = 0.0f  ;
+		  float inc = TWO_PI/90;
+		  int rRadio ;
+		  int rradio = 400;
+		  boolean dibujado= false; 
+		  int radio,Ax,Ay;
+		  //constructor de la clase
+		  Circulo (int _radio,int _Ax,int _Ay) {
+		    this.radio = _radio;
+		    this.Ax = _Ax;
+		    this.Ay =_Ay;
+		    this.rRadio = PApplet.parseInt(random(radio,rradio));
+		  }
+		  public void draw(int colorr) {
+		    //variante while dibuja de golpe
+		    while (a <  TWO_PI){
+		    dibujaCirculo();
+		     //  rotate(0.5);
+		    dibujaElipse(colorr);
+		      a = a + inc;
+		    }
+		    a =0;
+		    radio++;
+
+		    if (radio > rradio) {
+		      dibujado = true;
+		    }
+		  }
+
+		  public void dibujaCirculo(){
+
+		    // strokeWeight(1); 
+		    //    ahora recorremos la longitud de r para ir creando puntos que recorran todo el areadel circulo
+		    for (int i = 0; i < radio; i = i+100) {
+
+		      float xx = cos(a)*i+Ax; 
+		      float yy = sin(a)*i+Ay; 
+		      float xx2 = cos(a)*(i+10)+Ax; 
+		      float yy2 = sin(a)*(i+10)+Ay; 
+		      fill(0,30);
+		      noStroke();
+		      ellipse(xx,yy,5,5);
+		     
+		       }
+
+		  }
+		  public void dibujaElipse(int colorr){
+		   noStroke();
+		    int tam = 2;
+		    fill(colorr);
+		    ellipse(cos(a)*radio+Ax, sin(a)*radio+Ay,tam,tam);
+		  }
+		}
+
+	/*public boolean surfaceTouchEvent(MotionEvent event) {
+		return super.surfaceTouchEvent(event);
+	}*/
+
+	public void mousePressed() {
+			inicia();
+	}
+
+	// -----------------------------------------------------------------------------------------
+	// Override the parent (super) Activity class:
+	// States onCreate(), onStart(), and onStop() aren't called by the sketch.
+	// Processing is entered
+	// at the 'onResume()' state, and exits at the 'onPause()' state, so just
+	// override them as needed:
+
+	public void onResume() {
+		super.onResume();
+		println("RESUMED! (Sketch Entered...)");
+		// Create our Notification Manager:
+		gNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		// Create our Notification that will do the vibration:
+		gNotification = new Notification();
+		gNotification.defaults |= Notification.DEFAULT_SOUND;
+		gNotification.vibrate = gVibrate;
+
+	}
+
+	/*-----------------------------------------------------------------------------*/
+	/*------------------------ ADD IN ECLIPSE--------------------------------------*/
+	/*-----------------------------------------------------------------------------*/
+
+	/* pick a color */
+	private void showColor() {
+	}
+
+	/*------------------------ getScreen-----------------------------------*/
+	private void getScreen() {
+
+		String state = Environment.getExternalStorageState();
+
+		if (Environment.MEDIA_MOUNTED.equals(state)) {
+			/* como podemos leer la tarjeta grabamos */
+			String folder_path = getResources().getString(R.string.folder_path);
+			String name_path = getResources().getString(R.string.name_path);
+			String ext_path = getResources().getString(R.string.ext_path);
+			File extStore = Environment.getExternalStorageDirectory();
+
+			java.util.Date date = Calendar.getInstance().getTime();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+			String today = sdf.format(date);
+
+			String SD_PATH = extStore.getAbsolutePath() + folder_path
+					+ name_path + today + "." + ext_path;
+			save(SD_PATH);
+			println("------salvamos imagen------" + SD_PATH);
+			Toast.makeText(
+					getApplicationContext(),
+					getResources().getString(R.string.msg_saveImageOk)
+							+ SD_PATH, Toast.LENGTH_LONG).show();
+			// TODO para que sea visible en galery
+
+			/* */
+
+		} else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+			/* esta solo modo lectura */
+			// We can only read the media
+			Toast.makeText(getApplicationContext(),
+					getResources().getString(R.string.msg_saveImageFail),
+					Toast.LENGTH_LONG).show();
+		} else {
+			Toast.makeText(getApplicationContext(),
+					getResources().getString(R.string.msg_saveImageFail),
+					Toast.LENGTH_LONG).show();
+		}
+
+	}
+
+	/*------------------------ CREAMOS ACCIONES DE BOTON MENU-----------------------------------*/
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.app_menu, menu);
+		return true;
+	}
+
+	/**
+	 * Opens the file manager to pick a directory.
+	 */
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		/*
+		 * case R.id.settings: Intent intent2 = new Intent(this,
+		 * Preferences.class); startActivityForResult(intent2, EDIT_PREFS);
+		 * return true;
+		 */
+		case R.id.color:
+			//dibujar = false;
+			// new ColorPickerDialog(this, this, mInitialColor).show();
+			return true;
+		case R.id.fade:
+			fade.setFade();
+			return true;
+		case R.id.save:
+			getScreen();
+			return true;
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	public int sketchWidth() {
+		return displayWidth;
+	}
+
+	public int sketchHeight() {
+		return displayHeight;
+	}
+
+	public class Preferences extends PreferenceActivity {
+
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+
+			// Load the preferences from an XML resource
+			addPreferencesFromResource(R.xml.preferences);
+		}
+
+	}
+
+}
